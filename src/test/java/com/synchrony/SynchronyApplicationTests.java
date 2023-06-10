b93a7fc57b9a71e30f4b0bfdf3158bbd2f9ae842
Sunit Seth <sunitseth@gmail.com>
2023-06-10 16:10:19 +0530

    first commit-

---
+++ src/test/java/com/synchrony/SynchronyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synchrony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
